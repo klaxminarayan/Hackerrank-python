1a7f423d65c146e8e282effe6dfa76d878007b9d
klaxminarayan <laxminarayan9845@gmail.com>
2020-12-28 11:14:23 +0530

    Updating Math Sub-Domain

---
+++ Math/Triangle Quest 2.py
@@ -0,0 +1,12 @@
+for i in range(1,int(input())+1):
+    print(((10**i-1)//9)**2)
+"""
+It makes a series of geometric progression
+1^2
+11^2
+111^2
+
+sum = ((10^n) - 1)//9
+
+We need to print square so sum^2
+"""
